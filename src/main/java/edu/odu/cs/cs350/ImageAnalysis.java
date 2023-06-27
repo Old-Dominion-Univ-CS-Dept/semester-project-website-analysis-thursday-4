1403c1a52d4896553b0424ce699332665a98a2ee
cs-cbonh002 <cbonh002@odu.edu>
2023-06-27 10:33:19 -0400

    Update TestImageAnalysis.java and ImageAnalysis.java.
    Add imports for testing to TestImageAnalysis.java and add comments to
    ImageAnalysis.java that give a description of what the class should do
    according to the assignment.

--- src/main/java/edu/odu/cs/cs350/ImageAnalysis.java
+++ src/main/java/edu/odu/cs/cs350/ImageAnalysis.java
@@ -1,2 +1,8 @@
-package edu.odu.cs.cs350
+package edu.odu.cs.cs350; 
 
+// The system must be able to extract the images (<img ...> tags) from a page. 
+// Images should be classified as internal or external. 
+
+// For each internal image, the file size shall be recorded.
+// For each page, the URI of each image should be recorded. 
+// For each image, the local pages on which it is referenced shall be recorded. 
--- src/test/java/edu/odu/cs/cs350/TestImageAnalysis.java
+++ src/test/java/edu/odu/cs/cs350/TestImageAnalysis.java
@@ -1,3 +1,17 @@
-package edu.odu.cs.cs350
+package edu.odu.cs.cs350; 
 
 // This will be my test file for testing ImageAnalysis.java
+
+import org.junit.Test; 
+
+import static org.junit.Assert.*; 
+
+
+public class TestImageAnalysis
+{
+    @Test
+    public void testImage()
+    {
+        fail("This test needs to be implemented."); 
+    }
+}
