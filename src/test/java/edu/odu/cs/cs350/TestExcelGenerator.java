7aafed861a5cf4b438230dc22c66e935f59781d9
iv-g <igarr001@odu.edu>
2023-07-07 23:26:08 -0400

    moved filegen files to respective directories

---
+++ src/test/java/edu/odu/cs/cs350/TestExcelGenerator.java
@@ -0,0 +1,10 @@
+package edu.odu.cs.cs350;
+
+public class TestExcelGenerator {
+    public void testExcelGeneration(){
+        // Generate the Excel file
+        // Read the generated file
+        // Get the first sheet
+        // Verify the data in the sheet
+    }
+}
