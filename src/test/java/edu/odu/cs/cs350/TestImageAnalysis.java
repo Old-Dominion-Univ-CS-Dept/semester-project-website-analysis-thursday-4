1e51006cbf14be76e4a87e74fc0d84010edbbd7c
cs-cbonh002 <cbonh002@odu.edu>
2023-07-08 20:44:36 -0400

    Change to imports to fix compilation issues with gradle.

--- src/test/java/edu/odu/cs/cs350/TestImageAnalysis.java
+++ src/test/java/edu/odu/cs/cs350/TestImageAnalysis.java
@@ -2,9 +2,18 @@ package edu.odu.cs.cs350;
 
 // This will be my test file for testing ImageAnalysis.java
 
-import org.junit.Test; 
-
-import static org.junit.Assert.*; 
+import org.junit.jupiter.api.Test;
+import static org.junit.jupiter.api.Assertions.*;
+import java.io.File;
+import java.io.BufferedReader;
+import java.io.FileReader;
+import java.io.IOException;
+import java.util.Arrays;
+import java.util.List;
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.HashMap;
+import java.util.Map;
 
 
 public class TestImageAnalysis
