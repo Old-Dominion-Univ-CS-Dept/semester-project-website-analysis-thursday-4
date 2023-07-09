68a928fe47eee140b33253a8e769e838cca433f7
cs-cbonh002 <cbonh002@odu.edu>
2023-07-09 16:17:02 -0400

    Add two classes that need to be implemented in future.

---
+++ src/main/java/edu/odu/cs/cs350/HTMLDocumentBuilder.java
@@ -0,0 +1,15 @@
+/**
+ * This HTMLDocumentBuilder class will be responsible for 
+ * extracting all tags from a single file containing HTML content. 
+ * This is where out HTML parsing logic will exist. 
+ */
+package edu.odu.cs.cs350;
+
+/**
+ * @author bonham36
+ *
+ */
+public class HTMLDocumentBuilder 
+{
+    // Class to be implemented in future sprint. 
+}
---
+++ src/main/java/edu/odu/cs/cs350/WebsiteBuilder.java
@@ -0,0 +1,16 @@
+/**
+ * The WebsiteBuilder class will be responsible for collecting all 
+ * information needed to create a Website object:
+ *      i. one local directory path
+ *     ii. one or more URLs
+ */
+package edu.odu.cs.cs350;
+
+/**
+ * @author bonham36
+ *
+ */
+public class WebsiteBuilder 
+{
+    // Class will be implemented in future sprint. 
+}
