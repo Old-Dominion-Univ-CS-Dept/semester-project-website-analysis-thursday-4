c4d000a38fb8dc7526ff61ca05f02f6567d9b0d5
Zachery Elledge <zack931@hotmail.com>
2023-07-11 18:55:17 -0400

    Start of CLIoutput

---
+++ src/main/java/edu/odu/cs/cs350/CLIOutput.java
@@ -0,0 +1,16 @@
+/* 
+ * This CLIOutput class will print the names of created files
+ * to the command line interface 1 per line.
+ */
+
+
+package edu.odu.cs.cs350;
+
+/*
+ * @author Zelle001
+ */
+
+public class CLIOutput {
+    
+    
+}
---
+++ src/test/java/edu/odu/cs/cs350/TestCLIOutput.java
@@ -0,0 +1,21 @@
+package edu.odu.cs.cs350;
+
+import org.junit.jupiter.api.Test;
+import static org.junit.jupiter.api.Assertions.*;
+import java.io.File;
+import java.io.BufferedReader;
+import java.io.FileReader;
+import java.io.IOException;
+import java.util.Arrays;
+import java.util.List;
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.HashMap;
+import java.util.Map;
+
+
+public class TestCLIOutput {
+    
+    //@Test
+
+}
