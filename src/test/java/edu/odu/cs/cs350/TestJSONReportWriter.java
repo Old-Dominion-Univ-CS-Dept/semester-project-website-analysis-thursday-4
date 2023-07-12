b5ae559de133a32b10492674b48f49dc014e8dc3
iv-g <igarr001@odu.edu>
2023-07-12 19:32:47 -0400

    added dummy data to test for the writeReport method to ouput JSON report

--- src/test/java/edu/odu/cs/cs350/TestJSONReportWriter.java
+++ src/test/java/edu/odu/cs/cs350/TestJSONReportWriter.java
@@ -48,7 +48,61 @@ public void testGetOutputFile() {
 
         // Create some dummy report data
         Map<String, Object> reportData = new HashMap<>();
-        reportData.put("testKey", "testValue");
+        reportData.put("basePath", "/path/to/local/copy");
+        reportData.put("urls", Arrays.asList("http://www.url1.com", "https://www.url2.com"));
+    
+        List<Map<String, Object>> pages = new ArrayList<>();
+        Map<String, Object> page = new HashMap<>();
+        page.put("path", "./some/path/to/file1.html");
+        page.put("imageCount", Map.of("local", 5, "external", 2));
+        page.put("jsCount", Map.of("local", 12, "external", 0));
+        page.put("cssCount", Map.of("local", 2, "external", 1));
+        page.put("imagePaths", Arrays.asList("path/local/image1.svg", "https://upload.wikimedia.org/wikipedia/commons/2/24/LEGO_logo.svg"));
+        page.put("scriptPaths", Collections.singletonList("path/to/local/script.js"));
+        page.put("cssPaths", Collections.singletonList("someLocalStyle.css"));
+        page.put("linkCount", Map.of("intra-page", 5, "intra-site", 2, "external", 1));
+        pages.add(page);
+        reportData.put("pages", pages);
+
+        List<Map<String, Object>> images = new ArrayList<>();
+        Map<String, Object> image = new HashMap<>();
+        image.put("path", "path/to/image/1.jpg");
+        image.put("pageCount", 2);
+        image.put("usedOn", Arrays.asList("page1.htm", "page2.htm"));
+        images.add(image);
+        reportData.put("images", images);
+
+        Map<String, Object> files = new HashMap<>();
+        List<Map<String, Object>> archives = new ArrayList<>();
+        Map<String, Object> archive = new HashMap<>();
+        archive.put("path", "files/archive1.zip");
+        archive.put("size", "2 MiB");
+        archives.add(archive);
+        files.put("archive", archives);
+
+        List<Map<String, Object>> videos = new ArrayList<>();
+        Map<String, Object> video = new HashMap<>();
+        video.put("path", "files/video1.mkv");
+        video.put("size", "200 MiB");
+        videos.add(video);
+        files.put("video", videos);
+
+        List<Map<String, Object>> audios = new ArrayList<>();
+        Map<String, Object> audio = new HashMap<>();
+        audio.put("path", "audioFile7.flac");
+        audio.put("size", "10.2 MiB");
+        audios.add(audio);
+        files.put("audio", audios);
+
+        List<Map<String, Object>> others = new ArrayList<>();
+        Map<String, Object> other = new HashMap<>();
+        other.put("path", "files/test.cpp");
+        other.put("size", "0.05 MiB");
+        others.add(other);
+        files.put("other", others);
+
+        reportData.put("files", files);
+
 
         // Write the report
         writer.writeReport(reportData);
@@ -56,7 +110,7 @@ public void testGetOutputFile() {
         // Read the output file and check its contents
         try (BufferedReader reader = new BufferedReader(new FileReader("report.json"))) {
             String line = reader.readLine();
-            assertTrue(line.contains("\"testKey\":\"testValue\""));
+            assertTrue(line.contains("\"basePath\":\"/path/to/local/copy\""));
         } catch (IOException e) {
             fail("Error reading output file: " + e.getMessage());
         }
