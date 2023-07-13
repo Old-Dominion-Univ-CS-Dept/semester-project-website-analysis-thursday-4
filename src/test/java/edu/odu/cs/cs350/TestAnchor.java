2474673df74249a372ef0259a22a2e30658baa25
iv-g <igarr001@odu.edu>
2023-07-12 21:25:26 -0400

    Created additional test for Anchor class

--- src/test/java/edu/odu/cs/cs350/TestAnchor.java
+++ src/test/java/edu/odu/cs/cs350/TestAnchor.java
@@ -9,4 +9,10 @@ public class TestAnchor {
         Anchor anchor = new Anchor("<a href=\"http://example.com\">Example</a>");
         assertEquals("http://example.com", anchor.getHref());
     }
+
+    @Test
+    public void testGetText() {
+        Anchor anchor = new Anchor("<a href=\"http://example.com\">Example</a>");
+        assertEquals("Example", anchor.getText());
+    }
 }
