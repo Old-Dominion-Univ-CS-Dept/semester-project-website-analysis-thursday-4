cd3b19241f47ce023672308f570f59592e2f1476
iv-g <igarr001@odu.edu>
2023-07-13 11:52:07 -0400

    Refactored Anchor to extend Resource and added enum for linktype

--- src/main/java/edu/odu/cs/cs350/Anchor.java
+++ src/main/java/edu/odu/cs/cs350/Anchor.java
@@ -2,11 +2,34 @@ package edu.odu.cs.cs350;
 
 import java.util.regex.*;
 
-public class Anchor {
+/**
+ * This Anchor class extends the Resource class.
+ * It extracts the href attribute and the text between the opening and closing tags
+ * and classifies the link as intra-page, intra-site, or external.
+ * 
+ */
+
+ public class Anchor extends Resource {
     private String htmlRepresentation;
     private String href;
     private String text;
+    private LinkType linkType;
 
+    /**
+     * Enum for the type of link: intra-page, intra-site, or external.
+     */
+    public enum LinkType {
+        INTRA_PAGE,
+        INTRA_SITE,
+        EXTERNAL
+    }
+
+    /**
+     * Constructor for the Anchor class.
+     * @param htmlRepresentation The HTML representation of the anchor tag.
+     * @param currentUrl The URL of the current page.
+     * @param siteUrl The base URL of the site.
+     */
     public Anchor(String htmlRepresentation) {
         this.htmlRepresentation = htmlRepresentation;
         Pattern hrefPattern = Pattern.compile("href=\"(.*?)\"");
@@ -19,6 +42,14 @@ public class Anchor {
         if (textMatcher.find()) {
             this.text = textMatcher.group(1);
         }
+        
+            /**
+             * TODO: Update this section once siteUrl is defined
+             * If the href starts with "#", it's an intra-page link.
+             * If the href starts with siteUrl, it's an intra-site link.
+             * Otherwise, it's an external link.
+             * 
+             */
     }
 
     public String getHtmlRepresentation() {
@@ -31,6 +62,10 @@ public class Anchor {
 
     public String getText() {
         return text;
+    }
+
+    public LinkType getLinkType() {
+        return linkType;
     }
 }
 
