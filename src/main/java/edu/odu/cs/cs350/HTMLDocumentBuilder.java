70d9891e6b9f21d43208722fa3db4f8117e3aa8b
cs-cbonh002 <cbonh002@odu.edu>
2023-07-14 11:05:51 -0400

    Add file TestHTMLDocumentBuilder.java. Other changes see below.
    Add tests to TestHTMLDocumentBuilder.java.
    Add implementation to HTMLDocumentBuilder.java so tests will compile and
    pass.
    Add information to images.html for testing.

--- src/main/java/edu/odu/cs/cs350/HTMLDocumentBuilder.java
+++ src/main/java/edu/odu/cs/cs350/HTMLDocumentBuilder.java
@@ -5,11 +5,86 @@
  */
 package edu.odu.cs.cs350;
 
+import java.io.BufferedReader;
+import java.util.List;
+import java.util.ArrayList;
+import java.util.stream.Collectors;
+
+import org.jsoup.Jsoup;
+import org.jsoup.nodes.Document;
+import org.jsoup.nodes.Element;
+import org.jsoup.select.Elements;
+
 /**
+ * This class takes in different HTML tags and attributes
+ * and extracts all tags of the given type. Currently, I am
+ * focusing on extracting images only. Other tags will be 
+ * supported as the implementation expands. 
+ * 
  * @author bonham36
  *
  */
 public class HTMLDocumentBuilder 
 {
-    // Class to be implemented in future sprint. 
+    private String theTag; 
+    private String theAttribute; 
+    
+    /*
+     * Default constructor for HTMLBuilder, may be implemented later.
+     */    
+    public HTMLDocumentBuilder()
+    {
+        
+    }
+    
+    /* 
+     * Constructor that sets the current tag and attribute being extracted.
+     * 
+     * @param tag: the current tag being extracted. 
+     * 
+     * @param attribute: the current attribute being extracted. 
+     */ 
+    public HTMLDocumentBuilder(String tag, String attribute)
+    {
+        this.theTag = tag; 
+        this.theAttribute = attribute; 
+    }
+    
+    /*
+     * Extract all HTML image tags.
+     * 
+     * @param: HTML source code. 
+     * 
+     * @return: list of all image tags. 
+     */    
+    public List<Element> extractImages(BufferedReader htmlSource)
+    {
+        String htmlAsString = htmlSource.lines()
+                .collect(Collectors.joining(System.lineSeparator())); 
+        
+        return this.extractImages(htmlAsString); 
+    }
+    
+    
+    /*
+     * Extract all HTML image tags.
+     * 
+     * @param: HTML source code. 
+     * 
+     * @return: list of all image tags. 
+     */  
+    public List<Element> extractImages(String htmlSource)
+    {
+        Document doc = Jsoup.parse(htmlSource);
+        Elements elements = doc.select(this.theTag);
+
+        List<Element> elementList = new ArrayList<>();
+        for (Element elm : elements) {
+            elementList.add(elm);
+        }
+
+        return elementList;
+    }
+    
+    
 }
---
+++ src/test/java/edu/odu/cs/cs350/TestHTMLDocumentBuilder.java
@@ -0,0 +1,59 @@
+/**
+ * This file will test the functions of the HTMLDocumentBuilder class.
+ */
+package edu.odu.cs.cs350;
+
+import org.junit.jupiter.api.TestMethodOrder;
+import org.junit.jupiter.api.MethodOrderer;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.BeforeEach;
+
+import static org.junit.jupiter.api.Assertions.*;
+import static org.hamcrest.MatcherAssert.assertThat;
+import static org.hamcrest.Matchers.*;
+
+import org.hamcrest.core.IsNull;
+
+import java.util.List;
+import java.util.ArrayList;
+import java.util.Iterator;
+
+import java.io.BufferedReader;
+import java.io.FileReader;
+import java.io.FileNotFoundException;
+import java.io.IOException;
+
+import org.jsoup.nodes.Document;
+import org.jsoup.nodes.Element;
+/**
+ * @author bonham36
+ *
+ */
+public class TestHTMLDocumentBuilder 
+{
+    @Test
+    public void testExtractImage()
+    {
+        String htmlSource = "<img src=\"img_girl.jpg\" alt=\"Girl in a jacket\" width=\"500\" height=\"600\">"; 
+        
+        HTMLDocumentBuilder tester = new HTMLDocumentBuilder("img", "src"); 
+        
+        List<Element> images = tester.extractImages(htmlSource); 
+        
+        assertThat(images, is(notNullValue())); 
+        assertThat(images.size(), equalTo(1)); 
+    }
+    
+    @Test
+    public void testExtractImages() throws IOException, FileNotFoundException
+    {
+        BufferedReader htmlSource = new BufferedReader(new FileReader("src/test/resources/images.html")); 
+        
+        HTMLDocumentBuilder tester = new HTMLDocumentBuilder("img", "src"); 
+        
+        List<Element> images = tester.extractImages(htmlSource); 
+        
+        assertThat(images, is(notNullValue())); 
+        assertThat(images.size(), equalTo(5)); 
+    }
+}
