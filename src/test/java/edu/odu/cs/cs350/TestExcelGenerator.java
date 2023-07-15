2cd8cc47ec81cec786177e105ff026690e64bb10
Victor Olabiyi <olabiyivictord@gmail.com>
2023-07-15 01:22:24 -0400

    Excel File Generated("summary")

--- src/test/java/edu/odu/cs/cs350/TestExcelGenerator.java
+++ src/test/java/edu/odu/cs/cs350/TestExcelGenerator.java
@@ -1,5 +1,7 @@
 package edu.odu.cs.cs350;
 
+import org.testng.annotations.Test;
+
 public class TestExcelGenerator {
     public void testExcelGeneration(){
         // Generate the Excel file
@@ -7,4 +9,13 @@ public class TestExcelGenerator {
         // Get the first sheet
         // Verify the data in the sheet
     }
+
+    @Test
+    void testName() {
+        ExcelGenerator excelGen = new ExcelGenerator();
+
+        
+    }
+
+    
 }
