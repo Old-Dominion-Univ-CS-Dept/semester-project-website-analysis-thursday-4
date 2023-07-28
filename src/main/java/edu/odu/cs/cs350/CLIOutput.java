2fd4caa03ddf792ae5aa0ec6ecd728726d302929
Zachery Elledge <zelle001@odu.edu>
2023-07-28 18:16:40 -0400

    Minor changes

--- src/main/java/edu/odu/cs/cs350/CLIOutput.java
+++ src/main/java/edu/odu/cs/cs350/CLIOutput.java
@@ -1,17 +1,13 @@
-/* 
- * This CLIOutput class will print the names of created files
- * to the command line interface 1 per line.
- */
-
-
 package edu.odu.cs.cs350;
 
-/*
- * @author Zelle001
- */
 
  import java.io.File;
  import java.io.FilenameFilter;
+
+ /* 
+ * This CLIOutput class will print the names of created files
+ * to the command line interface 1 per line.
+ */
 
 
 public class CLIOutput {
