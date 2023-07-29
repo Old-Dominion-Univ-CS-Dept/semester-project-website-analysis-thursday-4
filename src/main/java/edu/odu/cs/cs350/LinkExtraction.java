494a9866a9a6242461e4ac6d0cb951bae0dc23fd
Baeduh <kylemunoz4@gmail.com>
2023-07-28 22:53:37 -0400

    create LinkExtraction and Test

---
+++ src/main/java/edu/odu/cs/cs350/LinkExtraction.java
@@ -0,0 +1,12 @@
+package edu.odu.cs.cs350;
+
+import java.util.regex.MatchResult;
+import java.util.regex.Pattern;
+
+public class LinkExtraction extends Anchor{
+    
+
+    /*Constructior for the LinkExtraction Class
+     * 
+     */
+}
