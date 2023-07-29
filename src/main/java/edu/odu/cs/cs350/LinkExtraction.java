709f6688dbde24d7ff9a8ed6e5e94c15abdc52b5
Baeduh <kylemunoz4@gmail.com>
2023-07-29 18:26:11 -0400

    Establisg a pattern to determine intrapage

--- src/main/java/edu/odu/cs/cs350/LinkExtraction.java
+++ src/main/java/edu/odu/cs/cs350/LinkExtraction.java
@@ -1,9 +1,9 @@
-// package edu.odu.cs.cs350;
+ package edu.odu.cs.cs350;
 
-// import java.util.regex.MatchResult;
-// import java.util.regex.Pattern;
+ import java.util.regex.Matcher;
+ import java.util.regex.Pattern;
 
-// public class LinkExtraction extends Anchor{
+ public class LinkExtraction extends Anchor{
     
 
 //     /*Constructior for the LinkExtraction Class
@@ -14,7 +14,7 @@
 //      {
 //         location = Locality.EXTERNAL;
 
-//         /*I want to extract linkgs(anchor tags) from a page 
+//         /*I want to extract links(anchor tags) from a page 
 //          * Classify them as
 //          * Intra-Page (link to another location on the page)
 //          * Intra-site (link to another location on the same site)
@@ -24,6 +24,23 @@
 //         String urlchecker = 
 //         Pattern urlPattern = Pattern.com
 
-//      }
+public LinkExtraction(String htmlRepresentation) {
+        super(htmlRepresentation);
+        //TODO Auto-generated constructor stub
+    }
 
-// }
+//      }
+public static String LinkTypeExtraction(String link){
+    /// Establish a pattern to check if hte link starts with a hash #
+    Pattern patternIntraPage = Pattern.compile("#,*$");
+    Matcher matcherIntraPage = matcherIntraPage.matcher(link);
+
+    /// Establish a pattern to check if a link start with /
+   // Pattern patternIntraSite = Pattern.compile("^/.*$");
+
+
+
+}
+
+
+ }
