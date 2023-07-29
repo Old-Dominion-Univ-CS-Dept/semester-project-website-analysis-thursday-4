822fe0fd80593bb96df17bd45d9c928b3417a6d5
Baeduh <kylemunoz4@gmail.com>
2023-07-29 00:35:36 -0400

    Added comments

--- src/main/java/edu/odu/cs/cs350/LinkExtraction.java
+++ src/main/java/edu/odu/cs/cs350/LinkExtraction.java
@@ -9,4 +9,21 @@ public class LinkExtraction extends Anchor{
     /*Constructior for the LinkExtraction Class
      * 
      */
+
+     public LinkExtraction()
+     {
+        location = Locality.EXTERNAL;
+
+        /*I want to extract linkgs(anchor tags) from a page 
+         * Classify them as
+         * Intra-Page (link to another location on the page)
+         * Intra-site (link to another location on the same site)
+         * External (usually a url)
+         */
+
+        String urlchecker = 
+        Pattern urlPattern = Pattern.com
+
+     }
+
 }
