42b4bf6c7cbe27c2a84d63741d3bde83d2a3ba62
cs-cbonh002 <cbonh002@odu.edu>
2023-08-01 19:47:07 -0400

    Edit documentation.

--- src/main/java/edu/odu/cs/cs350/CLIOutput.java
+++ src/main/java/edu/odu/cs/cs350/CLIOutput.java
@@ -4,7 +4,7 @@ package edu.odu.cs.cs350;
  import java.io.File;
  import java.io.FilenameFilter;
 
- /* 
+ /** 
  * This CLIOutput class will print the names of created files
  * to the command line interface 1 per line.
  */
