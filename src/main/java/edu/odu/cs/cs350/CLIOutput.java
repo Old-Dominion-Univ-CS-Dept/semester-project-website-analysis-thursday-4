22bb531d7353869f8b101d219693a30c4a331f23
Zachery Elledge <zelle001@odu.edu>
2023-08-01 19:47:21 -0400

    Javadoc

--- src/main/java/edu/odu/cs/cs350/CLIOutput.java
+++ src/main/java/edu/odu/cs/cs350/CLIOutput.java
@@ -4,7 +4,7 @@ package edu.odu.cs.cs350;
  import java.io.File;
  import java.io.FilenameFilter;
 
- /* 
+ /**
  * This CLIOutput class will print the names of created files
  * to the command line interface 1 per line.
  */
@@ -12,7 +12,7 @@ package edu.odu.cs.cs350;
 
 public class CLIOutput {
 
-    /*
+    /**
      * Mainly created to for test purposes.
      */
     public static void basicOut(){
@@ -21,24 +21,25 @@ public class CLIOutput {
     }
     
 
+    
     public static void out(String[] args){
 
-    /*
+    /**
      * String to hold the directory.
      */
         String directory;
 
-     /*
+     /**
      * Assigning directory the return for the  currentDirectory
      */
         directory = currentDirectory();
 
-     /*
+     /**
      * Assigning the value of getJson to a file[].
      */
         File[] fJSON = getJSON(directory);
 
-     /*
+     /**
      * Using FJSON to output the name to CLI and linebreak.
      */
         for(File file : fJSON) {
@@ -46,24 +47,24 @@ public class CLIOutput {
 
         }
 
-     /*
+     /**
      *  Assigning the value of getTXT to a file[].
      */
         File[] fTXT = getTXT(directory);
 
-    /*
+    /**
      * Using FTXT to output the name to CLI and linebreak.
      */    
         for(File file : fTXT) {
             System.out.println(file.getName() + System.lineSeparator());
         }
 
-     /*
+     /**
      * Assigning the value of getXLSX to a file[] variable.
      */
         File[] fXLSX = getXLSX(directory);
 
-     /*
+     /**
      * Using FTXT to output the name to CLI and linebreak.
      */ 
         for(File file : fXLSX) {
@@ -72,14 +73,14 @@ public class CLIOutput {
 
     }
 
-    /*
+    /**
      * This is passed the directory variable and is used to search for 
      * any .json files and return them.
      */
     public static File[] getJSON(String Directory) {
         File dir = new File(Directory);
 
-     /*
+     /**
      * This is used to filter through directory for a specific file ending with.json
      */     
         File[] fList = dir.listFiles(new FilenameFilter() {
@@ -90,14 +91,14 @@ public class CLIOutput {
         return fList;
     }
 
-      /*
+      /**
      * This is passed the directory variable and is used to search for 
      * any .txt files and return them.
      */
     public static File[] getTXT(String Directory) {
         File dir = new File(Directory);
 
-    /*
+    /**
      * This is used to filter through directory for a specific file ending with .txt
      */ 
         File[] fList = dir.listFiles(new FilenameFilter() {
@@ -108,14 +109,14 @@ public class CLIOutput {
         return fList;
     }
 
-      /*
+      /**
      * This is passed the directory variable and is used to search for 
      * any .xlsx files and return them.
      */
     public static File[] getXLSX(String Directory) {
         File dir = new File(Directory);
 
-     /*
+     /**
      * This is used to filter through directory for a specific file ending with .xlsx
      * and will return the file name as a string.
      */
@@ -127,7 +128,8 @@ public class CLIOutput {
         return fList;
     }
 
-    /*
+   
+    /**
      * This is used to get the current directory and return it.
      */
     public static String currentDirectory(){
@@ -135,6 +137,10 @@ public class CLIOutput {
         return directory;
     }
 
+    /**
+     * @param fileName
+     * @throws Exception
+     */
     public static void CLIOutFromFileMaker(String fileName) throws Exception {
 
         System.out.println(fileName + "/n");
