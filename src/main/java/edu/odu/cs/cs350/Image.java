1010c53079aa4514fccae1d8972c2160a484b6aa
cs-cbonh002 <cbonh002@odu.edu>
2023-08-02 08:42:25 -0400

    Update documentation.

--- src/main/java/edu/odu/cs/cs350/Image.java
+++ src/main/java/edu/odu/cs/cs350/Image.java
@@ -1,12 +1,9 @@
 package edu.odu.cs.cs350; 
 
-// The system must be able to extract the images (<img ...> tags) from a page. 
-// Images should be classified as internal or external. 
-
-// For each internal image, the file size shall be recorded.
-// For each page, the URI of each image should be recorded. 
-// For each image, the local pages on which it is referenced shall be recorded. 
-
+/**
+ * This class is used to create new Image objects during 
+ * extraction and analysis. 
+ */
 public class Image extends Resource
 {
     /* 
