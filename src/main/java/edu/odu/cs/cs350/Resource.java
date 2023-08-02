918ff964c706765ad17b4da4e9c7a70f7b0e4823
cs-cbonh002 <cbonh002@odu.edu>
2023-08-02 14:30:34 -0400

    Update documentation.

--- src/main/java/edu/odu/cs/cs350/Resource.java
+++ src/main/java/edu/odu/cs/cs350/Resource.java
@@ -3,9 +3,10 @@ package edu.odu.cs.cs350;
 import java.net.URL;
 import java.nio.file.Path;
 
-// Resource class from outline that will be needed for what is extracted from 
-// by the HTMLDocumentBuilder class. 
-
+/**
+ * Resource class from outline that will be needed for what is extracted from 
+ * by the HTMLDocumentBuilder class. 
+ */
 public class Resource
 {
     private ResourceKind typeOfResource; 
