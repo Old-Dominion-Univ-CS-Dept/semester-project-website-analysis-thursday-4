dd52671747096412611fda8b9c531151dd2684de
iv-g <igarr001@odu.edu>
2023-08-02 22:14:06 -0400

    corrected formatting on jsonreportwriter

--- src/main/java/edu/odu/cs/cs350/JSONReportWriter.java
+++ src/main/java/edu/odu/cs/cs350/JSONReportWriter.java
@@ -4,8 +4,10 @@ package edu.odu.cs.cs350;
 import java.io.BufferedWriter;
 import java.io.File;
 import java.io.IOException;
-import java.io.Writer;
+import java.net.URL;
+import java.util.ArrayList;
 import java.util.HashMap;
+import java.util.List;
 import java.util.Map;
 import java.io.FileWriter;
 
@@ -51,7 +53,96 @@ public class JSONReportWriter extends ReportWriter {
         return outputFile;
     }
 
+
     /**
+     * Converts the Website data into a JSON string.
+     *
+     * @param website A Website object to be converted into a JSON string.
+     * @return  The JSON string representation of the Website data.
+     */
+    private String convertToJson(Website website) {
+        Map<String, Object> websiteMap = new HashMap<>();
+    
+        websiteMap.put("basePath", website.getBasePath().toString());
+
+        List<String> urls = new ArrayList<>();
+        for (URL url : website.getUrls()) {
+            urls.add(url.toString());
+        }
+        websiteMap.put("urls", urls);
+
+        List<Map<String, Object>> documentsData = new ArrayList<>();
+        for (HTMLDocument document : website.getHtmlDocuments()) { 
+            documentsData.add(documentToMap(document));
+        }
+        websiteMap.put("documents", documentsData);
+
+        Map<String, Object> args = new HashMap<>();
+        args.put(JsonWriter.PRETTY_PRINT, true);
+        args.put(JsonWriter.TYPE, false);
+
+        return JsonWriter.objectToJson(websiteMap, args);
+    }
+
+    /**
+     * Converts a Resource object into a map.
+     * 
+     * @param resource The Resource to be converted.
+     * @return A map containing the properties of the Resource.
+     */
+    
+    private Map<String, Object> resourceToMap(Resource resource) {
+        Map<String, Object> resourceMap = new HashMap<>();
+
+        resourceMap.put("path", resource.getPath().toString());
+        resourceMap.put("type", resource.getTypeOfResource().toString());
+        resourceMap.put("classification", resource.getClass().toString());
+        resourceMap.put("size", Long.toString(resource.getSizeOfFile()));
+
+        resourceMap.put("typeOfResource", resource.getTypeOfResource());
+        resourceMap.put("location", resource.getLocation());
+        resourceMap.put("sizeOfFile", resource.getSizeOfFile());
+        
+        resourceMap.put("url", resource.getUrl().toString());
+
+        return resourceMap;
+    }
+
+     /**
+     * Converts a list of Resources into a list of maps.
+     * 
+     * @param document An HTMLDocument object.
+     * @return A document map.
+     */
+    private Map<String, Object> documentToMap(HTMLDocument document) {
+        Map<String, Object> documentMap = new HashMap<>();
+
+        documentMap.put("scripts", resourcesToListOfMaps(document.getScrips()));
+        documentMap.put("stylesheets", resourcesToListOfMaps(document.getStylesheets()));
+        documentMap.put("images", resourcesToListOfMaps(document.getImages()));
+        documentMap.put("anchors", resourcesToListOfMaps(document.getAnchors()));
+
+        return documentMap;
+    }
+
+
+    /**
+     * Converts a list of Resources into a list of maps.
+     * 
+     * @param resources The list of Resources to be converted.
+     * @return A list of maps, where each map represents a Resource.
+     */
+    private List<Map<String, Object>> resourcesToListOfMaps(List<Resource> resources) {
+        List<Map<String, Object>> listOfMaps = new ArrayList<>();
+
+        for (Resource resource : resources) {
+            listOfMaps.add(resourceToMap(resource));
+        }
+
+        return listOfMaps;
+    }
+
+     /**
      * Overrides the writeReport method from the ReportWriter class.
      * This method receives a Website object, converts it to a JSON string,
      * and writes it to the output file.
@@ -59,44 +150,42 @@ public class JSONReportWriter extends ReportWriter {
      * @param website A Website object to be written into the file.
      * @throws IOException If an I/O error occurs.
      */
-    
 
+    @Override
+    public void writeReport(Website website) throws IOException {
+        if (!outputFile.exists()) {
+            outputFile.getParentFile().mkdirs(); // Create parent directories if not exist
+            outputFile.createNewFile(); 
+        }
+
+        try (BufferedWriter bufferedWriter = new BufferedWriter(new FileWriter(outputFile))) {
+            String jsonString = convertToJson(website);
+            writeToFile(bufferedWriter, jsonString);
+        }
+    }
         /**
-     * Converts the Website data into a JSON string.
-     *
-     * @param website A Website object to be converted into a JSON string.
-     * @return  The JSON string representation of the Website data.
-     */
-    private String convertToJson(Website website) {
-        Map<String, Object> args = new HashMap<>();
-        args.put(JsonWriter.TYPE, false);
-        args.put(JsonWriter.PRETTY_PRINT, true); // Enable pretty print for readable JSON output
-        return JsonWriter.objectToJson(website, args);
-    }
+         * Converts an array of ASCII values into a string.
+         *
+         * @param asciiValues The array of ASCII values to be converted.
+         * @return The resulting string after conversion.
+         */
 
-
-     @Override
-     public void writeReport(Website website) throws IOException {
-         // Check if file exists, and if not, create it
-         if (!outputFile.exists()) {
-             outputFile.getParentFile().mkdirs(); // Create parent directories if not exist
-             outputFile.createNewFile(); // Create the file itself
-         }
- 
-         try (BufferedWriter bufferedWriter = new BufferedWriter(new FileWriter(outputFile))) {
-             String jsonString = convertToJson(website);
-             writeToFile(bufferedWriter, jsonString);
-         }
-     }
-    /**
-     * Writes a string to a file using a BufferedWriter.
-     *
-     * @param writer The BufferedWriter to be used for writing the string.
-     * @param jsonString The string to be written to the file.
-     * @throws IOException If an I/O error occurs.
-     */
-    private void writeToFile(BufferedWriter writer, String jsonString) throws IOException {
-        writer.write(jsonString);
-        writer.write("\n");  
-    }
+        private String asciiToString(Object[] asciiValues) {
+            StringBuilder sb = new StringBuilder();
+            for (Object value : asciiValues) {
+                sb.append((char)((int)value));
+            }
+            return sb.toString();
+        }
+        /**
+         * Writes a string to a file using a BufferedWriter.
+         *
+         * @param writer The BufferedWriter to be used for writing the string.
+         * @param jsonString The string to be written to the file.
+         * @throws IOException If an I/O error occurs.
+         */
+        private void writeToFile(BufferedWriter writer, String jsonString) throws IOException {
+            writer.write(jsonString);
+            writer.write("\n");  
+        }
 }
