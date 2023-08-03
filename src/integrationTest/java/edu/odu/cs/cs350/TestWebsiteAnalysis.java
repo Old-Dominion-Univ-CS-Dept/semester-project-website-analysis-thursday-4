d1d2f745f71ac94b264c451e1c6c6f1f116d6dc9
cs-cbonh002 <cbonh002@odu.edu>
2023-08-03 12:03:38 -0400

    Update to suport Integration Testing.

---
+++ src/integrationTest/java/edu/odu/cs/cs350/TestWebsiteAnalysis.java
@@ -0,0 +1,36 @@
+package edu.odu.cs.cs350; 
+
+import org.junit.jupiter.api.TestMethodOrder;
+import org.junit.jupiter.api.MethodOrderer;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.ValueSource;
+
+import static org.hamcrest.MatcherAssert.assertThat;
+import static org.junit.jupiter.api.Assertions.*;
+import static org.hamcrest.Matchers.*;
+
+import org.hamcrest.core.IsNull;
+
+import java.util.List;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.Iterator;
+
+import java.util.stream.Stream;
+import static java.util.stream.Collectors.toList;
+
+/**
+ * This is an integration test. Even though we are using JUnit... this is not a
+ * unit test.
+ */
+
+ public class TestWebsiteAnalysis
+ {
+    @Test
+    public void testOne()
+    {
+        fail("test needs to be implemented."); 
+    }
+ }
